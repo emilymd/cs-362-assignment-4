e47cd27363d6c6d4743095a7f5f4730e2b35807d
Amanda <Amanda@MacBook-Pro.local>
2019-03-18 22:00:24 -0700

    fixed sonar pulse, now only works when you sink a ship of opponent

--- project-team-9-sprint-3/src/main/java/assets/game.js
+++ project-team-9-sprint-3/src/main/java/assets/game.js
@@ -132,6 +132,17 @@ function cellClick() {
     } else {
 
         if (sonarIsChecked){
+            let opBoard = document.getElementById("opponent");
+            for (i=0; i<10; i++) {
+                for (j=0; j<10; j++) {
+                    let cell = opBoard.rows[i].cells[j];
+                    if (cell.classList.contains("sunk")){
+                        numShipsSunk++;
+                    }
+
+                }
+             }
+
 
             /*//get all attacks on the player board? or on the opponent board? may need to use
             var opBoard = document.getElementById("opponent");
